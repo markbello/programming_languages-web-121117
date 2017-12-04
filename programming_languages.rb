5fcd74c0df37a2cdb7c9a5b325935a1e0a4a8a04
Mark Bello <mcbello51286@gmail.com>
2017-12-04 17:50:00 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -3,6 +3,6 @@ def reformat_languages(languages)
   type_array = []
   style_array = []
   reformatted_languages = {}
-  languages.each {|type| type.each{|lang| reformatted_languages[lang] = [lang]}}
+  languages.each {|type,val| val.each{|lang, val| reformatted_languages[lang] = [val]}}
 
 end
