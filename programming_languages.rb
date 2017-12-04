ac59f1b9becca3d44f7370638e249a63c29c5f79
Mark Bello <mcbello51286@gmail.com>
2017-12-04 18:14:08 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -7,7 +7,7 @@ def reformat_languages(languages)
   reformatted_languages = {}
   languages.each{|key1, value|
     value.each{|key2,value|
-      if reformatted_languages[key2][:style] == nil
+      if reformatted_languages[key2] == nil
         reformatted_languages[key2] = value
         reformatted_languages[key2][:style] = []
         reformatted_languages[key2][:style].push(key1)
