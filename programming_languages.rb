2255b45628d0341e42c0e0338d60ccc3d43fd023
Mark Bello <mcbello51286@gmail.com>
2017-12-04 18:15:10 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,17 @@
+require "pry"
+
 def reformat_languages(languages)
-  # your code here
+  reformatted_languages = {}
+  languages.each{|key1, value|
+    value.each{|key2,value|
+      if reformatted_languages[key2] == nil
+        reformatted_languages[key2] = value
+        reformatted_languages[key2][:style] = []
+        reformatted_languages[key2][:style].push(key1)
+      else
+        reformatted_languages[key2][:style].push(key1)
+      end
+      }
+    }
+  reformatted_languages
 end
